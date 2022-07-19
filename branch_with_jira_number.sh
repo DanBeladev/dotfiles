f2d2346f739b1c864ad1f5c9649863ba54620408
DanBeladev <dan.beladev@cyberark.com>
2022-07-19 15:09:33 +0300

    .

--- branch_with_jira_number.sh
+++ branch_with_jira_number.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 jira_key="$1"
-branch="feature/ANUN-"${jira_key}""
-echo "create branch: ${branch}"
-if [ -n "$1" ]
-then
+branch="feature/ANUN-$jira_key"
+echo "create branch: $branch"
+if [[ -n "$1" ]]
+then 
     git checkout develop
     git pull
-    git checkout -b ${branch}
-    git push --set-upstream origin ${branch}
+    git checkout -b $branch
+    git push --set-upstream origin $branch
 fi
